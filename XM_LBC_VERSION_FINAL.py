91e67c3150e4bc6bfa6f1f7107d1bc5950c6e362
carloskaa <ccarom@unal.edu.co>
2023-03-23 12:45:54 -0500

    correcion datos lbc

--- XM_LBC_VERSION_FINAL.py
+++ XM_LBC_VERSION_FINAL.py
@@ -11,7 +11,7 @@ import numpy as np
 import xlsxwriter
 from datetime import timedelta
 
-inputs = pd.read_excel('datos_LBC.xlsx',sheet_name='festivos')
+inputs = pd.read_excel('Datos_LBC.xlsx',sheet_name='festivos')
 festivos = inputs['Fecha'].tolist()
 df_pruebas =  pd.read_excel(r'C:\Users\Asus\OneDrive - julia-rd.com.co\Direccion MEM\Operacion_MEM\06. RESULTADOS_PRUEBAS\Consolidado_PRUEBAS.xlsx')
 df_pruebas['desconexion'] = 1
